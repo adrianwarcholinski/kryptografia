305bfef51aa1da55b2cb9c14625e88c38d3f4f83
Adi <adrianwarcholinski9@gmail.com>
2018-10-12 21:05:51 +0200

    Komunikat testowy od Adriana.

--- trunk/projekt/program/src/main.cpp
+++ trunk/projekt/program/src/main.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int main() {
+    // test - Adrian
     cout << "Algorytm szyfrowania AES. \n Autorzy:\n- Aleksandra Ruta\n-Adrian Warcholinski\n- Kamil Piatkowski" << endl;
     return 0;
 }

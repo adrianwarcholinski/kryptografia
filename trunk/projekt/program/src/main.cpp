48a1bf7d122b4b089ac17e10f448d3ae24a1250e
Kamil <kamil12p@gmail.com>
2018-10-12 21:12:53 +0200

    Rozpoczęte wczytywanie danych

--- trunk/projekt/program/src/main.cpp
+++ trunk/projekt/program/src/main.cpp
@@ -3,8 +3,20 @@
 #include <fstream>
 
 using namespace std;
+class obiekt_do_szyfrowania{
+private:
+    char* tresc;
+    bool opcja;
+public:
+    obiekt_do_szyfrowania(){
+        cout<<"Prosze podac metode wprowadzania danych:\n1. Tekst wprowadzany z klawiatury\n2. Tekst wczytywany z pliku\nWpisz wybraną opcje:    ";
+        cin>>opcja;
+        cout<<opcja;
+        }
 
+};
 int main() {
     cout << "Algorytm szyfrowania AES. \n Autorzy:\n- Aleksandra Ruta\n-Adrian Warcholinski\n- Kamil Piatkowski" << endl;
+    obiekt_do_szyfrowania ob;
     return 0;
 }

702abe1aa843075a6fd099518150b59d0984b189
Kamil <kamil12p@gmail.com>
2018-10-12 17:45:18 +0200

    Inicjalizacja programu

---
+++ trunk/projekt/program/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    cout << "Algorytm szyfrowania AES. \n Autorzy:\n- Aleksandra Ruta\n-Adrian Warcholinski\n- Kamil Piatkowski" << endl;
+    return 0;
+}

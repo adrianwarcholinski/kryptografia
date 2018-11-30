87937ccb4449251b42f8b82d00dff802f93f6d07
Kamil <kamil12p@gmail.com>
2018-11-30 11:34:44 +0100

    Math update

--- include/Math.h
+++ include/Math.h
@@ -2,12 +2,15 @@
 #define PROGRAM_MATH_H
 
 #include "BigInt.h"
+#include <bitset>
 
 class Math {
 private:
     Math();
 public:
 static BigInt fastPower(BigInt x, BigInt m, BigInt n);
+static bitset<1024> dec2Bin(BigInt x);
+static bool isPrime(BigInt x);
 };
 
 
--- src/Math.cpp
+++ src/Math.cpp
@@ -6,6 +6,15 @@
 
 #include "Math.h"
 
-BigInt Math::fastPower(BigInt x) {
+BigInt Math::fastPower(BigInt x, BigInt m, BigInt n) {
+
     return BigInt();
+}
+
+bitset<1024> Math::dec2Bin(BigInt x) {
+
+}
+
+bool Math::isPrime(BigInt x) {
+    return false;
 }
